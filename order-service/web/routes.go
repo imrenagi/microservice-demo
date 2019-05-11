9bf48d67f621476835b486c9b09aea80ad1814f3
imrenagi <imre.nagi2812@gmail.com>
2019-05-11 13:06:00 +0700

    Fix routes for local

--- order-service/web/routes.go
+++ order-service/web/routes.go
@@ -1,6 +1,6 @@
 package web
 
 func (s Server) routes() {
-	s.Router.HandleFunc("/orders", s.CreateOrder()).Methods("POST")
-	s.Router.HandleFunc("/orders", s.GetOrders()).Methods("GET")
+	s.Router.HandleFunc("/orders/", s.CreateOrder()).Methods("POST")
+	s.Router.HandleFunc("/orders/", s.GetOrders()).Methods("GET")
 }
